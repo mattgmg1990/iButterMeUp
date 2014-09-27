d1816762cf132bbcacb76087b0874f61b1a34a38
Matt Garnes <matt@mattgarnes.com>
2014-09-26 23:42:21 -0700

    Initial Commit

---
+++ Butter Me Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Butter Me Up
+//
+//  Created by Matthew Garnes on 9/26/14.
+//  Copyright (c) 2014 Matt Garnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
