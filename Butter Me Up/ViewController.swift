be7d933bb6eaf5df5f6bc393b4af274962642694
Matt Garnes <matt@mattgarnes.com>
2014-09-28 13:40:27 -0700

    Add gesture recognizer for taps on the TextView.

--- Butter Me Up/ViewController.swift
+++ Butter Me Up/ViewController.swift
@@ -11,12 +11,30 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var complimentTextView : UITextView!
+    @IBOutlet var tvTapGestureListener : UIGestureRecognizer!
+    var butterModel : ButterModel!
+    
+    /*
+    class Subscriber : NSObject  {
+        override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: NSDictionary!, context: ) {
+            println("observeValueForKey: (object)")
+            let tv = UITextView(object);
+            CGFloat topCorrect = ([tv bounds].size.height - [tv contentSize].height * [tv zoomScale])/2.0;
+            topCorrect = ( topCorrect < 0.0 ? 0.0 : topCorrect );
+            tv.contentOffset = (CGPoint){.x = 0, .y = -topCorrect};
+        }
+    }
+    */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        butterModel = ButterModel()
     }
-
+    
+    @IBAction func showNextCompliment(sender: AnyObject) {
+        complimentTextView.text = butterModel.getCompliment()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
